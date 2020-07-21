ffcee580238521bd8dba7296558405e2c62b3f7a
vince <2632170361@qq.com>
2020-07-21 16:54:13 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import { ToyReact, Component } from "./ToyReact";
+
+class MyComponent extends Component {
+  render() {
+    return <div>
+      <span>hello</span>
+      <span>world!</span>
+      <div>
+        {true}
+        {this.children}
+      </div>
+    </div>
+  }
+  setAttribute(name, value) {
+    this[name] = value;
+  }
+  mountTo(parent) {
+    let vdom = this.render();
+    vdom.mountTo(parent);
+  }
+}
+
+let a = <MyComponent name="name" id="id"></MyComponent>
+
+ToyReact.render(
+  a,
+  document.body,
+);
